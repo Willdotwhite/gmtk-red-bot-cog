9285f3e8d37ea13d08b9582b642197f29d2044b1
WillDotWhite <willcopsonwhite@gmail.com>
2026-06-03 16:05:29 +0100

    Remove use of redbot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
+import os
+import logging
 from discord import app_commands
-from redbot.core import commands
-from redbot.core.bot import Red
+from discord.ext import commands
 
 # =========================================================================
 # CONFIGURATION
@@ -18,10 +19,11 @@ TARGET_CHANNELS = ["general", "help", "support"]
 TARGET_KEYWORDS = ["help", "stuck", "error", "broken", "how to"]
 # =========================================================================
 
+
 class JamCommands(commands.Cog):
     """Game Jam Management Commands and Auto-Responders."""
 
-    def __init__(self, bot: Red):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         # Pre-process lists to lowercase for efficient case-insensitive matching
         self.target_channels = [c.lower() for c in TARGET_CHANNELS]
@@ -44,7 +46,7 @@ class JamCommands(commands.Cog):
         return True
 
     # =========================================================================
-    # KEYWORD AUTO-RESPONDER (Listens in multiple channels for multiple words)
+    # KEYWORD AUTO-RESPONDER
     # =========================================================================
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -64,159 +66,140 @@ class JamCommands(commands.Cog):
                 except discord.HTTPException:
                     pass
 
+    # =========================================================================
+    # SLASH COMMANDS
+    # =========================================================================
     @app_commands.command(name="ai", description="Print AI usage rules.")
-    @app_commands.default_permissions()
-    async def ai(self, interaction: discord.Interaction):
+    async def cmd_ai(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**AI Rules**: You must not use generative AI to make art or audio assets for your game, or your itch.io page. This will lead to your game being disqualified. Full rules are [on the itch.io page](https://itch.io/jam/gmtk-jam-2026)", ephemeral=False)
 
     @app_commands.command(name="jaminfo", description="Print general jam information.")
-    @app_commands.default_permissions()
-    async def jaminfo(self, interaction: discord.Interaction):
+    async def cmd_jaminfo(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Jam Info**: All information you need about the jam can be found on itch.io: https://itch.io/jam/gmtk-jam-2026", ephemeral=False)
 
     @app_commands.command(name="jamruling", description="Print jam rules and criteria.")
-    @app_commands.default_permissions()
-    async def jamruling(self, interaction: discord.Interaction):
+    async def cmd_jamruling(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Jam Ruling**: Discord moderators can *not* make any ruling regarding jam rules, so if you are uncertain about something we ask you to read the jam rules and use your own judgement: https://itch.io/jam/gmtk-jam-2026\nIf you’re still unsure if something is OK or not, we recommend that you err on the side of safety and refrain from doing it! <:linkThumbsUp:948315490076487742>", ephemeral=False)
 
     @app_commands.command(name="itch", description="Print the itch.io submission link.")
-    @app_commands.default_permissions()
-    async def itch(self, interaction: discord.Interaction):
+    async def cmd_itch(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("<:gmtk:407473480703934464> https://itch.io/jam/gmtk-jam-2026 <:gmtk:407473480703934464>", ephemeral=False)
 
     @app_commands.command(name="hype", description="Print hype message/warning.")
-    @app_commands.default_permissions()
-    async def teamfinder(self, interaction: discord.Interaction):
+    async def cmd_hype(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Please keep hype, countdowns and spam to https://discord.com/channels/248204508960653312/1270435373666926613!**", ephemeral=False)
 
     @app_commands.command(name="teamfinder", description="Print team finding instructions.")
-    @app_commands.default_permissions()
-    async def teamfinder(self, interaction: discord.Interaction):
+    async def cmd_teamfinder(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**If you're looking for a team, head over to https://findyourjam.team/gmtk and https://discord.com/channels/248204508960653312/1270438349114703983 !**", ephemeral=False)
 
     @app_commands.command(name="theme", description="Print jam theme info.")
-    @app_commands.default_permissions()
-    async def teamfinder(self, interaction: discord.Interaction):
+    async def cmd_theme(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("## **THE THEME FOR THE GMTK GAME JAME 2026 IS...**\n# *** ??? ***\n[Link to video here!](https://www.youtube.com/watch?v=z_kAvHKPWYo&ab_channel=GameMaker%27sToolkit)", ephemeral=False)
 
     @app_commands.command(name="health", description="Print health and wellness tips.")
-    @app_commands.default_permissions()
-    async def health(self, interaction: discord.Interaction):
+    async def cmd_health(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("We strongly recommend you take care of yourself during the jam! Make sure to: sleep well, take regular breaks and stay on top of eating and drinking. You make a better game when you're healthy, not when you're crunching!", ephemeral=False)
 
     @app_commands.command(name="jamtips", description="Print useful jam tips.")
-    @app_commands.default_permissions()
-    async def jamtips(self, interaction: discord.Interaction):
+    async def cmd_jamtips(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("Here are some important tips for having a great jam! https://discord.com/channels/248204508960653312/1379068723809222787/1399378216002785391", ephemeral=False)
 
     @app_commands.command(name="jamstart", description="Print jam countdown timer.")
-    @app_commands.default_permissions()
-    async def jamtips(self, interaction: discord.Interaction):
+    async def cmd_jamstart(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**The jam starts at <t:1784739600:f> (<t:1784739600:R>).**\n**You can view the rules and sign up here: <https://itch.io/jam/gmtk-jam-2026>**\n**Please keep countdowns to https://discord.com/channels/248204508960653312/1270435373666926613!**", ephemeral=False)
 
     @app_commands.command(name="extension", description="Print extension info.")
-    @app_commands.default_permissions()
-    async def jamtips(self, interaction: discord.Interaction):
+    async def cmd_extension(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**There are NO PLANS for time extension this year!**\n**The jam ends when the timer on <https://itch.io/jam/gmtk-jam-2026> is up!**", ephemeral=False)
 
     @app_commands.command(name="jamend", description="Print extension info.")
-    @app_commands.default_permissions()
-    async def jamtips(self, interaction: discord.Interaction):
+    async def cmd_jamend(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Submissions close on <t:1785085200:F> (<t:1785085200:R>).**\n**We strongly recommend you upload your game** ***before*** **the last hour! If something goes wrong in the last minutes of the jam, we will be unable assist you!**", ephemeral=False)
 
     @app_commands.command(name="build", description="Print game build instructions.")
-    @app_commands.default_permissions()
-    async def build(self, interaction: discord.Interaction):
+    async def cmd_build(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Don't forget to plan for the time it takes to build and test your game!**\n**You can upload multiple builds of your game to itch.io and update your game submission, so make sure you submit early! You can freely change which game files are part of your submission** ***up until the jam deadline***, **even after you've submitted the game!**", ephemeral=False)
 
     @app_commands.command(name="extraextension", description="Print extra extension info.")
-    @app_commands.default_permissions()
-    async def build(self, interaction: discord.Interaction):
+    async def cmd_extraextension(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**To account for issues with late submissions, the jam has received an extra** ***1 hour*** **added to the original submission time. The final time is at <t:1754244000:t> <t:1754244000:R>!**\n# This is not time for you to add extra features or bugfix. This is to help those who had slow builds or issues with submitting to itch.io get their games in.\n**If you have successfully submitted your game, congratulations! We recommend you use this time to relax, you made it!**", ephemeral=False)
 
     @app_commands.command(name="gamelink", description="Print game submission link info.")
-    @app_commands.default_permissions()
-    async def gamelink(self, interaction: discord.Interaction):
+    async def cmd_gamelink(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**Please do not link your itch.io game page outside of https://discord.com/channels/248204508960653312/1270438107296043122.**\n**If someone is requesting to play your game, please DM them instead.**", ephemeral=False)
 
     @app_commands.command(name="jamover", description="Print jam end info.")
-    @app_commands.default_permissions()
-    async def gamelink(self, interaction: discord.Interaction):
+    async def cmd_jamover(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**The jam submission deadline is now over! It is no longer possible to upload or edit submissions to the jam. If you did not manage to get your game submitted in time there is unfortunately nothing we can do at this stage.**\n**DO NOT DELETE YOUR GAME FILES - IT WILL NOT LET YOU UPLOAD NEW ONES, AND WE ARE UNABLE TO RESTORE THEM IF YOU DO! YOU WILL BE DISQUALIFIED!**\nFeel free to edit your game's itch.io page, thumbnail and description, but do not add links to other builds of your game!", ephemeral=False)
 
     @app_commands.command(name="closing", description="Print closing info.")
-    @app_commands.default_permissions()
-    async def gamelink(self, interaction: discord.Interaction):
+    async def cmd_closing(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("<:linkConfetti:922817487295361124> **THE GMTK Game Jam is now over! Thank you everyone for participating! The server will be closing for non-patrons <t:1754740800:R>! See you next year! ** <:linkConfetti:922817487295361124> \nOr, if you want to keep hanging around, please consider becoming a GMTK patron and gain access to the discord all year round!", ephemeral=False)
 
     @app_commands.command(name="r4r", description="Print Rate 4 Rate info.")
-    @app_commands.default_permissions()
-    async def r4r(self, interaction: discord.Interaction):
+    async def cmd_r4r(self, interaction: discord.Interaction):
         if await self._check_permissions(interaction):
             await interaction.response.send_message("**R4R (Rate for Rate)**: WORDS GO HERE PLS", ephemeral=False)
 
-# Required RedBot Cog setup function
-async def setup(bot: Red):
-    await bot.add_cog(JamCommands(bot))
 
 # =========================================================================
-# LOCAL DEVELOPMENT RUNNER
+# BOT SETUP & EXECUTION
 # =========================================================================
-# This block will ONLY run if you execute this script directly via Python.
-# It uses a local mock environment rather than loading into the full RedBot.
+class PlainDiscordBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True  # Required for the auto-responder
+        super().__init__(command_prefix="!", intents=intents)
+
+    async def setup_hook(self):
+        # Load the commands
+        await self.add_cog(JamCommands(self))
+        
+        # Command Syncing Logic
+        guild_id = os.environ.get("GUILD_ID")
+        if guild_id:
+            try:
+                target_guild = discord.Object(id=int(guild_id))
+                self.tree.copy_global_to(guild=target_guild)
+                await self.tree.sync(guild=target_guild)
+                logging.info(f"Synced slash commands directly to guild: {guild_id}")
+            except Exception as e:
+                logging.error(f"Failed to sync commands to guild {guild_id}: {e}")
+        else:
+            await self.tree.sync()
+            logging.info("Synced slash commands globally. (Note: Global syncs can take up to 1 hour to appear in Discord client).")
+
+    async def on_ready(self):
+        logging.info(f"Logged in successfully as {self.user} (ID: {self.user.id})")
+
+
 if __name__ == "__main__":
-    import logging
-    from discord.ext import commands as dpy_commands
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s: %(message)s')
     
-    # Configure basic logging to see commands/errors locally
-    logging.basicConfig(level=logging.INFO)
+    # Securely retrieve the token from the environment
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    
+    if not BOT_TOKEN:
+        logging.critical("BOT_TOKEN environment variable is not set. Exiting.")
+        exit(1)
 
-    # --- LOCAL TEST CONFIGURATION ---
-    LOCAL_BOT_TOKEN = "YOUR_REST_BOT_TOKEN_HERE"
-    LOCAL_GUILD_ID = 1470708876998344861  # Replace with your local test server/guild ID
-
-    class LocalDevBot(dpy_commands.Bot):
-        def __init__(self):
-            # Message content intent is required for the auto-responder to read messages
-            intents = discord.Intents.default()
-            intents.message_content = True 
-            super().__init__(command_prefix="!", intents=intents)
-
-        async def setup_hook(self):
-            # Load the exact same Cog that RedBot will use
-            await self.add_cog(JamCommands(self)) # type: ignore
-            
-            # Sync slash commands directly to your test guild for INSTANT updates.
-            # (Global syncs normally take up to an hour; this avoids that)
-            test_guild = discord.Object(id=LOCAL_GUILD_ID)
-            self.tree.copy_global_to(guild=test_guild)
-            await self.tree.sync(guild=test_guild)
-            print(f"\n[LOCAL DEV] Commands synced directly to test guild: {LOCAL_GUILD_ID}")
-
-        async def on_ready(self):
-            print(f"\n[LOCAL DEV] Logged in successfully as {self.user}!")
-            print("[LOCAL DEV] Ready for fast iteration. Press Ctrl+C to stop.\n")
-
-    if LOCAL_BOT_TOKEN == "YOUR_TEST_BOT_TOKEN_HERE":
-        print("\n❌ Please set LOCAL_BOT_TOKEN and LOCAL_GUILD_ID at the bottom of the script to run locally.\n")
-    else:
-        # Create and run our lightweight test bot
-        local_bot = LocalDevBot()
-        local_bot.run(LOCAL_BOT_TOKEN)
+    bot = PlainDiscordBot()
+    bot.run(BOT_TOKEN)
